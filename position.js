8108f35cc335aa990df8778d6b753cc24bcfca39
MihoUezono <30921737+MihoUezono@users.noreply.github.com>
2017-09-10 23:31:53 +0900

    Add files via upload

---
+++ position.js
@@ -0,0 +1,20 @@
+
+/* デバイスの傾きや方角の値が変化したとき */
+window.addEventListener('deviceorientation',function(event) {
+
+	/* カメラの位置を取得 */
+	var element = document.getElementById('camera'); 
+	var position = element.getAttribute('position')
+	
+	/* x,y,z軸の値 */
+	var str;
+	str = position.x + ", " + position.y + ", " + position.z ;
+	alert(str);
+	
+	/*
+	alert(position.x);
+	alert(position.y);
+	alert(position.z);
+	*/
+	
+});

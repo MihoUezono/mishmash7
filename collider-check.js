dec681d987bd84f00ccb1ef993223c4d10221d2c
MihoUezono <30921737+MihoUezono@users.noreply.github.com>
2017-11-12 13:28:27 +0900

    Add files via upload

---
+++ collider-check.js
@@ -0,0 +1,171 @@
+var t = 0;
+var isIntersect = false;
+
+// 開始時間の取得
+var start = new Date();
+
+// その他経過時間表示用変数
+var count = 120; //カウントダウンの数字を格納する変数
+var min = 0; //残り時間「分」を格納する変数
+var sec = 0; //残り時間「秒」を格納する変数
+var stp = null; //setInerval・clearInervalを制御する変数
+var i = 0; //ボタンの2回クリック等禁止イベントを制御する変数
+var nowTime = 0;
+var datet = 0;
+
+// 歩数カウント用変数
+var nowSteps = 0;
+
+//var cnt = 0;
+
+// 勝敗結果表示用変数
+//var result = '';
+
+var socketio = io.connect('http://54.178.168.53:3000/');
+
+socketio.on("getposition", function(player_id, cameraposition) {});
+socketio.on("setposition", function (player_id, cameraposition) { synchro(player_id, cameraposition); });
+socketio.on("setresult", function (player_id) { setmessage(player_id); });
+
+//socketio.emit("getposition", player_id);
+
+// ポジションをサーバに送信
+function getposition() {
+  var element = document.getElementById(player_id);
+  var cameraposition = element.getAttribute('position')
+  socketio.emit("getposition", player_id, cameraposition);
+}
+
+function synchro(player_id, cameraposition){
+
+  var opponent = document.getElementById(player_id);
+  //var opponentposition = opponent.getAttribute('position')
+  opponent.setAttribute('position', cameraposition);
+
+}
+
+// 対戦結果をサーバに送信
+function setresult() {
+  socketio.emit("setresult", player_id);
+}
+
+function setmessage(player_id){
+
+  if (player_id == 'camera'){
+    document.getElementById("result1").textContent = 'WINNER';
+    document.getElementById("result2").textContent = 'LOSER';
+  }
+  else{
+    document.getElementById("result1").textContent = 'LOSER';
+    document.getElementById("result2").textContent = 'WINNER';
+  }
+
+}
+
+//socketio.on("getposition", function (player_id, cameraposition) { 
+  //addMessage(cameraposition.value);
+//});
+
+
+AFRAME.registerComponent('collider-check', {
+  dependencies: ['raycaster'],
+  init: function () {
+    this.el.addEventListener('raycaster-intersection', function () {
+      isIntersect = true;
+      
+    });
+    this.el.addEventListener('raycaster-intersection-cleared', function () {
+      isIntersect = false;
+    });
+  }
+});
+
+function movePlayer() {
+
+  var camera = document.getElementById(player_id);
+
+    /* カメラの位置を取得 */
+    var position = camera.getAttribute('position')
+    var rotation = camera.getAttribute('rotation')
+
+    /* isIntersect,x,y,z軸の値 */
+    var str;
+    /*str = isIntersect + ", " + position.x + ", " + position.y + ", " + position.z ;*/
+    var camera1 = document.getElementById("camera");
+    var position1 = camera1.getAttribute('position');
+    str = position1.x + ", " + position1.y + ", " + position1.z ;
+
+    document.getElementById("cameraPos").textContent = str;
+
+    // ゴール判定
+    if ( position.z >= 90 ){
+    
+      if (rotation.x < -35 ){
+
+        if (camera && !isIntersect) {
+
+          var cameraposition = camera.getAttribute('position')
+          var camerarotation = camera.getAttribute('rotation')
+
+          cameraposition.x -= 0.04 * Math.sin(Math.PI * (camerarotation.y) / 180);
+          cameraposition.z -= 0.04 * Math.cos(Math.PI * (camerarotation.y) / 180);
+          camera.setAttribute('position', cameraposition);
+
+          // 歩数カウントのインクリメント
+          nowSteps ++;
+
+          // 歩数の表示
+          document.getElementById("steps").textContent = nowSteps;
+        }
+
+      }
+      
+     }
+    else {
+      // ゴール
+      //var audiomain = document.getElementById('audiomain');
+      //audiomain.components.sound.stopSound();
+   
+      //var audiogoal = document.getElementById('audiogoal');
+      //audiogoal.components.sound.playSound();
+      //count_stop();
+
+      setresult();
+
+    }
+
+  getposition();
+
+}
+
+function dispTime() {
+  // 現在時刻の取得
+  nowTime = new Date();
+
+  datet = parseInt((nowTime.getTime() - start.getTime()) / 1000);
+
+  min = parseInt((datet / 60) % 60);
+  sec = datet % 60;
+
+  // 数値が1桁の場合、頭に0を付けて2桁で表示する指定
+  if(min < 10) { min = "0" + min; }
+  if(sec < 10) { sec = "0" + sec; }
+
+  // フォーマットを指定
+  var timer = min + ':' + sec;
+
+  // 経過時間の表示
+  document.getElementById("time").textContent = timer;
+}
+
+function render() {
+
+  t += 0.01;
+  requestAnimationFrame(render);
+
+  movePlayer();
+
+  dispTime();
+
+}
+render();
